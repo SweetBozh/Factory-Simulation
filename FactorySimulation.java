ef5a18d8779ff5735132cc790b8ec3c08f09baa7
Podolph <nalin_hippo@hotmail.com>
2021-11-04 18:11:46 +0700

    Create Class FactorySimulation

---
+++ FactorySimulation.java
@@ -0,0 +1,10 @@
+/*Member 
+1. Warunyupa  Lerdsaeng        6313180
+2. Nalin      Suesangiamsakul  6313216*/
+
+class FactorySimulation{
+        public static void main(String[] args){
+
+
+    }
+}
